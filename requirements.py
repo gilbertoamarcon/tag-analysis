39c56aa89e8beea1a0efb76124c232da8dc383ab
Gilberto Marcon <gilbertoamarcon@gmail.com>
2017-12-08 13:36:53 -0800

    Features

--- requirements.py
+++ requirements.py
@@ -1,62 +1,50 @@
 #!/usr/bin/python
-import requests
 import string
 import sys
-import re
 import bibtexparser
 from collections import OrderedDict
-from itertools import combinations
 
 requirements = OrderedDict([
-	('nonlinear','Nonlinear time-varying variables'),
-	('linear','Linear time-varying variables'),
-	('continuous','Continuous variables'),
-	('temporal','Temporal constraints'),
-	('lim-com','Limited or no communication'),
-	('partial-obs','Partial observability'),
-	('nondet-outcomes','Non-deterministic action outcomes'),
 	('concurrent','Concurrent action execution'),
+	('nondet-outcomes','Non-deterministic action outcomes'),
+	('partial-obs','Partial observability'),
+	('lim-com','Limited or no communication'),
+	('temporal','Temporal constraints'),
+	('continuous','Continuous variables'),
+	('linear','Linear time-varying variables'),
+	('nonlinear','Nonlinear time-varying variables'),
 	('none','None of the above'),
 ])
 
-def get_planners(tag, bib):
-	ret_val = []
-	for k in bib:
-		if tag in bib[k]:
-			ret_val.append(k)
-	return ret_val
-
-def get_feature_based_table(requirements, bib):
-	table = '\\begin{table}[!ht]\n\centering\n\caption{Planning Requirements.}\n\label{tab:requirements}\n\\begin{tabular}{l|l}\n\\textbf{Requirement} &  \\textbf{Planners} \\\\ \cline{1-2}\n'
-	for r in requirements:
-		table += '\\textbf{' + requirements[r] + '} & \cite{' + ','.join(get_planners(r,bib)) + '} \\\\ \n'
-	table += '\end{tabular}\n\end{table}\n'
-	return table
-
 def get_planner_based_table(requirements,key_oriented):
+	if 'none' in requirements:
+		del requirements['none']
 	nrows = len(requirements)+1
 	alpha_indexes = list(string.ascii_lowercase)[:len(requirements)]
-	align = 'X | ' + '|'.join(['c' for i in range(nrows-1)])
+	align = 'V{3} X V{3}' + '|'.join(['c' for i in range(nrows-1)]) + 'V{3}'
 	reqs = '&'.join(['\\textbf{%c}'%r for r in alpha_indexes])
 	table = ''
 	table += '\\begin{table}[!ht]\n'
 	table += '\centering\n'
-	table += '\caption{Planning Requirements.'
+	table += '\caption[Planner Features.]{Planner Features.'
 	for n,r in enumerate(requirements):
 		table += ' %c) %s.' % (alpha_indexes[n], requirements[r])
 	table += '}\n'
 	table += '\label{tab:requirements}\n'
 	table += '\\begin{tabularx}{\linewidth}{%s}\n' % align
-	table += '\\textbf{Planner} & '+reqs+' \\\\ \cline{1-%d}\n' % nrows
-	for p in key_oriented:
+	table += '\hline \Cline{1pt}{1-%d}\n' % nrows
+	table += '\multirow{2}{*}{\\textbf{Planner}} & \multicolumn{%d}{c V{3}}{\\textbf{Features}} \\\\ \Cline{1pt}{2-%d}\n' % (nrows-1,nrows)
+	table += '& '+reqs+' \\\\ \hline \Cline{1pt}{1-%d}\n' % nrows
+	for p in reversed(sorted(key_oriented.keys())):
 		table += '\cite{'+ ','.join(key_oriented[p]) +'} & '
 		req = []
-		for r in requirements:
-			if r in p:
+		for r in p:
+			if r:
 				req.append('X')
 			else:
 				req.append('-')
-		table += ' & '.join(req) + ' \\\\ \cline{1-%d}\n' % nrows
+		table += ' & '.join(req) + ' \\\\ \hline\n'
+	table += '\Cline{1pt}{1-%d}\n' % nrows
 	table += '\end{tabularx}\n\end{table}\n'
 	return table
 
@@ -66,26 +54,35 @@ if len(sys.argv) < 2:
 else:
 	file_dir = sys.argv[1]
 
-# Getting BIB Dictionary
-with open(file_dir+'/bib.bib') as f:
-	entry_dict = bibtexparser.loads(f.read()).get_entry_dict()
+def load_bib(filename,requirements):
 
-# Getting tag sets
-bib = {}
-for key in entry_dict:
-	if 'keyword' in entry_dict[key]:
-		bib[key] = set(entry_dict[key]['keyword'].encode('ascii').split(', '))
+	# Getting the BIB Dictionary
+	with open(filename) as f:
+		entry_dict = bibtexparser.loads(f.read()).get_entry_dict()
 
-# Filtering
-for b in bib:
-	bib[b] &= set(requirements.keys())
+	# Getting tag sets
+	bib = OrderedDict()
+	for key in entry_dict:
+		if 'keyword' in entry_dict[key]:
+			reqs = set(entry_dict[key]['keyword'].encode('ascii').split(', ')) & set(requirements.keys())
+			if len(reqs) > 0:
+				bib[key] = reqs
+
+	# Enumerating
+	if 'none' in requirements:
+		del requirements['none']
+	for b in bib:
+		bib[b] = tuple([r in bib[b] for r in requirements])
+
+	return bib
+
+bib = load_bib(file_dir+'/bib.bib',requirements)
 
 key_oriented = {}
 for b in bib:
-	key = tuple(sorted(list(bib[b])))
-	if key not in key_oriented:
-		key_oriented[key] = set()
-	key_oriented[key].add(b)
+	if bib[b] not in key_oriented:
+		key_oriented[bib[b]] = set()
+	key_oriented[bib[b]].add(b)
 
 with open(file_dir+'/table.tex','w') as f:
 	f.write(get_planner_based_table(requirements,key_oriented))
